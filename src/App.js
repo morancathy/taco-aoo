0d56714c2b711d983bfa2e46c269f997c90f47a5
Cat <morancathy@gmail.com>
2021-07-22 21:31:12 -0500

    final styling. Submitting homework with hungry for more attempt commented out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./styles.css";
-
 import Taco from "./Taco";
 
 export default function App() {
   const [taco, setTaco] = useState({});
-  const [shell, setShell] = useState("");
+  const [shell, setShell] = useState('');
+  const searchInput = useRef(null);
 
   const getTaco = async () => {
     try {
@@ -13,58 +13,53 @@ export default function App() {
         "https://taco-randomizer.herokuapp.com/random/?full-tack=true"
       );
       const data = await response.json();
-      console.log("In App.js. line 16 console.log(data) gave me:", data);
-      setTaco({ ...data });
-
-      console.log(
-        "In App.js line 19 console.log(data.shell) gave me:",
-        data.shell
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getShell = async () => {
-    try {
-      const response = await fetch(
-        "https://taco-randomizer.herokuapp.com/contributors/shells/"
-      );
-      const data = await response.json();
-
+      console.log("App.js-16:", data);
+      console.log("App.js-17:", data.shell);
       setTaco({ ...data });
     } catch (err) {
       console.log(err);
     }
   };
 
-  // useEffect(() => {
-  //   getShell("naan");
-  // }, []);
+  /* The commented lines throughout this page is the "hungry for more" attempt.
+     I want to created a form that allows the user to enter a type of shell, then the page continues
+     to display random taco ingredients...BUT the shell option is what they typed in. */
 
-  const handleChange = (event) => {
-    setShell(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    getShell(shell);
-    setShell("");
-  };
+  // const getShell = async (searchItem) => {
+  //   try {
+  //     const response = await fetch(
+  //       `http://taco-randomizer.herokuapp.com/shells/${searchInput}`
+  //       // `https://taco-randomizer.herokuapp.com/?apikey=f2c395ca/contributors/shells/${searchItem}`
+  //     );
+  //     const data = await response.json();
+  //
+  //     setTaco({ ...data });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+  //
+  // const handleChange = (event) => {
+  //   console.log(searchInput)
+  //   setShell(event.target.ref);
+  // };
+  //
+  // const handleSubmit = (event, searchInput) => {
+  //   event.preventDefault();
+  //   getShell(searchInput);
+  //   setShell("");
+  // };
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      {/*<form onSubmit={handleSubmit}>
         <label htmlFor="shell"> Taco Shell: </label>
-        <input id="shell" type="text" value={shell} onChange={handleChange} />
+        <input id="shell" type="text" value={shell} ref={searchInput} onChange={handleChange} />
         <input type="submit" value="Find Shell" />
-      </form>
+      </form>*/}
 
-      <h1>Random Taco App</h1>
+      <h1 className="title">Random Taco!</h1>
       <button onClick={getTaco}>Click Me for Taco Fun</button>
-      {/* <Taco taco={taco} /> */}
-      {/* why do we need object.keys? Where is 'Object'?*/}
-      {/* Is there simiplier way to write below code? */}
       <h2>{Object.keys(taco).length ? <Taco taco={taco} /> : ""}</h2>
     </div>
   );
--- src/Taco.js
+++ src/Taco.js
@@ -3,13 +3,13 @@ import "./styles.css";
 export default function Taco(props) {
   return (
     <div className="tacos">
-      {console.log("In Taco.js,line6, console.log(props) gave me:", props)}
-      <h4 className="tacoName">{props.taco.base_layer.name} Taco</h4>
-      <h4>Shell: {props.taco.shell.name}</h4>
+      <h4><span>Base: </span> {props.taco.base_layer.name}</h4>
+      <p>Recipe: {props.taco.base_layer.recipe}</p>
+      <h4><span>Shell: </span> {props.taco.shell.name}</h4>
       <p>Recipe: {props.taco.shell.recipe}</p>
-      <h4>Mixin: {props.taco.mixin.name}</h4>
+      <h4><span>Mixin: </span>{props.taco.mixin.name}</h4>
       <p>Recipe: {props.taco.mixin.recipe}</p>
-      <h4>Condiment: {props.taco.condiment.name}</h4>
+      <h4><span>Condiment: </span>{props.taco.condiment.name}</h4>
       <p>Recipe: {props.taco.condiment.recipe}</p>
     </div>
   );
